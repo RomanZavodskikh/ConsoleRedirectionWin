1d0f1c7f03c7f5afebe51ddb32910014d0eca8d0
Roman Zavodskikh <roman-zavodskikh@parallels.mipt.ru>
2018-04-14 00:51:38 +0300

    Added 2 threads to client

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -7,18 +7,21 @@
 
 #define BUFSIZE 512
 
+DWORD WINAPI InstanceThreadClientServer(LPVOID);
+DWORD WINAPI InstanceThreadServerClient(LPVOID);
+
 int _tmain(int argc, TCHAR *argv[])
 {
     HANDLE hPipeClientServer, hPipeServerClient;
+    HANDLE hThreadClientServer, hThreadServerClient;
     HANDLE hHeap = GetProcessHeap();
     TCHAR* pchRequest = (TCHAR*)HeapAlloc(hHeap, 0, BUFSIZE * sizeof(TCHAR));
     TCHAR* pchReply = (TCHAR*)HeapAlloc(hHeap, 0, BUFSIZE * sizeof(TCHAR));
     BOOL   fSuccess = FALSE;
     DWORD  cbRead, cbToWrite, cbWritten, dwMode;
+    DWORD  dwThreadId = 0;
     LPTSTR lpszPipeClientServer = TEXT("\\\\.\\pipe\\clientserver");
     LPTSTR lpszPipeServerClient = TEXT("\\\\.\\pipe\\serverclient");
-
-    // Try to open a named pipe; wait for it, if necessary. 
 
     while (1)
     {
@@ -34,11 +37,10 @@ int _tmain(int argc, TCHAR *argv[])
 
                             // Break if the pipe handle is valid. 
 
-        if (hPipeClientServer != INVALID_HANDLE_VALUE)
+         if (hPipeClientServer != INVALID_HANDLE_VALUE)
             break;
 
         // Exit if an error other than ERROR_PIPE_BUSY occurs. 
-
         if (GetLastError() != ERROR_PIPE_BUSY)
         {
             _tprintf(TEXT("Could not open pipe. GLE=%d\n"), GetLastError());
@@ -46,13 +48,27 @@ int _tmain(int argc, TCHAR *argv[])
         }
 
         // All pipe instances are busy, so wait for 20 seconds. 
-
         if (!WaitNamedPipe(lpszPipeClientServer, 20000))
         {
             printf("Could not open pipe: 20 second wait timed out.");
             return -1;
         }
     }
+
+    hThreadClientServer = CreateThread(
+        NULL,              // no security attribute 
+        0,                 // default stack size 
+        InstanceThreadClientServer,    // thread proc
+        (LPVOID)hPipeClientServer,    // thread parameter 
+        0,                 // not suspended 
+        &dwThreadId);      // returns thread ID 
+
+    if (hThreadClientServer == NULL)
+    {
+        _tprintf(TEXT("CreateThread failed, GLE=%d.\n"), GetLastError());
+        return -1;
+    }
+    else CloseHandle(hThreadClientServer);
     // =================================================================
     while (1) 
     {
@@ -72,7 +88,6 @@ int _tmain(int argc, TCHAR *argv[])
             break;
 
         // Exit if an error other than ERROR_PIPE_BUSY occurs. 
-
         if (GetLastError() != ERROR_PIPE_BUSY)
         {
             _tprintf(TEXT("Could not open pipe(2). GLE=%d\n"), GetLastError());
@@ -80,16 +95,28 @@ int _tmain(int argc, TCHAR *argv[])
         }
 
         // All pipe instances are busy, so wait for 20 seconds. 
-
         if (!WaitNamedPipe(lpszPipeServerClient, 20000))
         {
             printf("Could not open pipe: 20 second wait timed out.");
             return -1;
         }
     }
+    hThreadServerClient = CreateThread(
+        NULL,              // no security attribute 
+        0,                 // default stack size 
+        InstanceThreadServerClient,    // thread proc
+        (LPVOID)hPipeServerClient,    // thread parameter 
+        0,                 // not suspended 
+        &dwThreadId);      // returns thread ID 
 
+    if (hThreadServerClient == NULL)
+    {
+        _tprintf(TEXT("CreateThread failed, GLE=%d.\n"), GetLastError());
+        return -1;
+    }
+    else CloseHandle(hThreadServerClient);
+    
     // The pipe connected; change to message-read mode. 
-
     dwMode = PIPE_READMODE_MESSAGE;
     fSuccess = SetNamedPipeHandleState(
         hPipeClientServer,    // pipe handle 
@@ -113,6 +140,65 @@ int _tmain(int argc, TCHAR *argv[])
         return -1;
     }
 
+    for (;;) {
+        Sleep(100);
+    }
+
+    CloseHandle(hPipeClientServer);
+    HeapFree(hHeap, 0, pchRequest);
+    HeapFree(hHeap, 0, pchReply);
+    return 0;
+}
+
+DWORD WINAPI InstanceThreadClientServer(LPVOID lpvParam)
+// This routine is a thread processing function to read from and reply to a client
+// via the open pipe connection passed from the main loop. Note this allows
+// the main loop to continue executing, potentially creating more threads of
+// of this procedure to run concurrently, depending on the number of incoming
+// client connections.
+{
+    HANDLE hHeap = GetProcessHeap();
+    TCHAR* pchRequest = (TCHAR*)HeapAlloc(hHeap, 0, BUFSIZE * sizeof(TCHAR));
+    TCHAR* pchReply = (TCHAR*)HeapAlloc(hHeap, 0, BUFSIZE * sizeof(TCHAR));
+
+    DWORD cbBytesRead = 0, cbReplyBytes = 0, cbWritten = 0;
+    BOOL fSuccess = FALSE;
+    HANDLE hPipe = NULL;
+
+    // Do some extra error checking since the app will keep running even if this
+    // thread fails.
+
+    if (lpvParam == NULL)
+    {
+        printf("\nERROR - Pipe Server Failure:\n");
+        printf("   InstanceThread got an unexpected NULL value in lpvParam.\n");
+        printf("   InstanceThread exitting.\n");
+        if (pchReply != NULL) HeapFree(hHeap, 0, pchReply);
+        if (pchRequest != NULL) HeapFree(hHeap, 0, pchRequest);
+        return (DWORD)-1;
+    }
+
+    if (pchRequest == NULL)
+    {
+        printf("\nERROR - Pipe Server Failure:\n");
+        printf("   InstanceThread got an unexpected NULL heap allocation.\n");
+        printf("   InstanceThread exitting.\n");
+        if (pchReply != NULL) HeapFree(hHeap, 0, pchReply);
+        return (DWORD)-1;
+    }
+
+    if (pchReply == NULL)
+    {
+        printf("\nERROR - Pipe Server Failure:\n");
+        printf("   InstanceThread got an unexpected NULL heap allocation.\n");
+        printf("   InstanceThread exitting.\n");
+        if (pchRequest != NULL) HeapFree(hHeap, 0, pchRequest);
+        return (DWORD)-1;
+    }
+
+    hPipe = (HANDLE)lpvParam;
+
+    // Loop until done reading
     while (1)
     {
         HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
@@ -120,62 +206,144 @@ int _tmain(int argc, TCHAR *argv[])
 
         fSuccess = ReadFile(
             hStdin,
-            pchRequest,
-            BUFSIZE * sizeof(*pchRequest),
-            &cbRead,
+            pchReply,
+            BUFSIZE * sizeof(*pchReply),
+            &cbReplyBytes,
             NULL);
 
         if (!fSuccess)
         {
-            _tprintf(TEXT("ReadFile from stdin failed. GLE=%d\n"), GetLastError());
-            return -1;
+            _tprintf(TEXT("ReadFile stdin failed, GLE=%d.\n"), GetLastError());
+            break;
         }
 
+        // Write the reply to the pipe. 
         fSuccess = WriteFile(
-            hPipeClientServer,                  // pipe handle 
-            pchRequest,             // message 
-            cbRead,              // message length 
-            &cbWritten,             // bytes written 
-            NULL);                  // not overlapped 
+            hPipe,        // handle to pipe 
+            pchReply,     // buffer to write from 
+            cbReplyBytes, // number of bytes to write 
+            &cbWritten,   // number of bytes written 
+            NULL);        // not overlapped I/O 
 
-        if (!fSuccess || cbRead != cbWritten)
+        if (!fSuccess || cbReplyBytes != cbWritten)
         {
-            _tprintf(TEXT("WriteFile to pipe failed. GLE=%d\n"), GetLastError());
-            return -1;
+            _tprintf(TEXT("InstanceThread WriteFile failed, GLE=%d.\n"), GetLastError());
+            break;
         }
+    }
 
-        // Read from the pipe. 
+    // Flush the pipe to allow the client to read the pipe's contents 
+    // before disconnecting. Then disconnect the pipe, and close the 
+    // handle to this pipe instance. 
 
+    FlushFileBuffers(hPipe);
+    DisconnectNamedPipe(hPipe);
+    CloseHandle(hPipe);
+
+    HeapFree(hHeap, 0, pchRequest);
+    HeapFree(hHeap, 0, pchReply);
+    return 1;
+}
+
+DWORD WINAPI InstanceThreadServerClient(LPVOID lpvParam)
+// This routine is a thread processing function to read from and reply to a client
+// via the open pipe connection passed from the main loop. Note this allows
+// the main loop to continue executing, potentially creating more threads of
+// of this procedure to run concurrently, depending on the number of incoming
+// client connections.
+{
+    HANDLE hHeap = GetProcessHeap();
+    TCHAR* pchRequest = (TCHAR*)HeapAlloc(hHeap, 0, BUFSIZE * sizeof(TCHAR));
+    TCHAR* pchReply = (TCHAR*)HeapAlloc(hHeap, 0, BUFSIZE * sizeof(TCHAR));
+
+    DWORD cbBytesRead = 0, cbReplyBytes = 0, cbWritten = 0;
+    BOOL fSuccess = FALSE;
+    HANDLE hPipe = NULL;
+
+    // Do some extra error checking since the app will keep running even if this
+    // thread fails.
+
+    if (lpvParam == NULL)
+    {
+        printf("\nERROR - Pipe Server Failure:\n");
+        printf("   InstanceThread got an unexpected NULL value in lpvParam.\n");
+        printf("   InstanceThread exitting.\n");
+        if (pchReply != NULL) HeapFree(hHeap, 0, pchReply);
+        if (pchRequest != NULL) HeapFree(hHeap, 0, pchRequest);
+        return (DWORD)-1;
+    }
+
+    if (pchRequest == NULL)
+    {
+        printf("\nERROR - Pipe Server Failure:\n");
+        printf("   InstanceThread got an unexpected NULL heap allocation.\n");
+        printf("   InstanceThread exitting.\n");
+        if (pchReply != NULL) HeapFree(hHeap, 0, pchReply);
+        return (DWORD)-1;
+    }
+
+    if (pchReply == NULL)
+    {
+        printf("\nERROR - Pipe Server Failure:\n");
+        printf("   InstanceThread got an unexpected NULL heap allocation.\n");
+        printf("   InstanceThread exitting.\n");
+        if (pchRequest != NULL) HeapFree(hHeap, 0, pchRequest);
+        return (DWORD)-1;
+    }
+
+    hPipe = (HANDLE)lpvParam;
+
+    // Loop until done reading
+    while (1)
+    {
+        HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+        // Read client requests from the pipe. This simplistic code only allows messages
+        // up to BUFSIZE characters in length.
         fSuccess = ReadFile(
-            hPipeServerClient,    // pipe handle 
-            pchReply,    // buffer to receive reply 
-            BUFSIZE * sizeof(TCHAR),  // size of buffer 
-            &cbRead,  // number of bytes read 
-            NULL);    // not overlapped 
+            hPipe,        // handle to pipe 
+            pchRequest,    // buffer to receive data 
+            BUFSIZE * sizeof(TCHAR), // size of buffer 
+            &cbBytesRead, // number of bytes read 
+            NULL);        // not overlapped I/O 
 
-        if (!fSuccess)
+        if (!fSuccess || cbBytesRead == 0)
         {
-            _tprintf(TEXT("ReadFile from pipe failed. GLE=%d\n"), GetLastError());
-            return -1;
+            if (GetLastError() == ERROR_BROKEN_PIPE)
+            {
+                _tprintf(TEXT("InstanceThread: client disconnected.\n"));
+            }
+            else
+            {
+                _tprintf(TEXT("InstanceThread ReadFile failed, GLE=%d.\n"), GetLastError());
+            }
+            break;
         }
 
         fSuccess = WriteFile(
             hStdout,
-            pchReply,
-            cbRead,
-            &cbWritten,
+            pchRequest,
+            cbBytesRead,
+            &cbReplyBytes,
             NULL);
 
-        if (!fSuccess || cbRead != cbWritten)
+        if (!fSuccess || cbBytesRead != cbReplyBytes)
         {
-            _tprintf(TEXT("WriteFile to stdout failed. GLE=%d\n"), GetLastError());
-            return -1;
+            _tprintf(TEXT("WriteFile stdout failed, GLE=%d.\n"), GetLastError());
+            break;
         }
     }
 
-    CloseHandle(hPipeClientServer);
+    // Flush the pipe to allow the client to read the pipe's contents 
+    // before disconnecting. Then disconnect the pipe, and close the 
+    // handle to this pipe instance. 
+
+    FlushFileBuffers(hPipe);
+    DisconnectNamedPipe(hPipe);
+    CloseHandle(hPipe);
+
     HeapFree(hHeap, 0, pchRequest);
     HeapFree(hHeap, 0, pchReply);
-
-    return 0;
+    return 1;
 }
